1cb9335196f6bf9bb00d2dd400b7d9f9695480a7
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-21 10:30:13 +0100

    spring security

--- src/main/java/pl/sda/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/pl/sda/springbootsecurity/config/SecurityConfig.java
@@ -5,13 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,9 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+                .permitAll();
     }
 
     @Autowired
