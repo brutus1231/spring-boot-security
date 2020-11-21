5de9fbfb01e9683e37eb6bc3cacae204411d69a7
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-21 10:29:41 +0100

    Initial commit

---
+++ src/test/java/pl/sda/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
